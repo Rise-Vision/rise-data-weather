e7e66196171d2f4efc6ebcad9d0c4e83a89e29e7
Alex Deaconu <alex.deaconu@gmail.com>
2019-06-26 12:30:57 -0400

    Use RiseElement as component base (#35)
    Remove redundant functionality
    
    Remove iron-ajax dependency

--- src/rise-data-weather.js
+++ src/rise-data-weather.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-console */
 
-import { PolymerElement } from "@polymer/polymer";
 import { timeOut } from "@polymer/polymer/lib/utils/async.js";
 import { Debouncer } from "@polymer/polymer/lib/utils/debounce.js";
+import { RiseElement } from "rise-common-component/src/rise-element.js";
 import { CacheMixin } from "rise-common-component/src/cache-mixin.js";
 
 import { weatherServerConfig } from "./rise-data-weather-config.js";
 import { version } from "./rise-data-weather-version.js";
 import { parseTinbu } from "./tinbu-parser.js";
 
-class RiseDataWeather extends CacheMixin( PolymerElement ) {
+class RiseDataWeather extends CacheMixin( RiseElement ) {
 
   static get properties() {
     return {
@@ -57,12 +57,6 @@ class RiseDataWeather extends CacheMixin( PolymerElement ) {
   }
 
   // Event name constants
-  static get EVENT_CONFIGURED() {
-    return "configured";
-  }
-  static get EVENT_START() {
-    return "start";
-  }
   static get EVENT_DATA_UPDATE() {
     return "data-update";
   }
@@ -101,6 +95,8 @@ class RiseDataWeather extends CacheMixin( PolymerElement ) {
   constructor() {
     super();
 
+    this._setVersion( version );
+
     this._weatherRequestRetryCount = 0;
     this._refreshDebounceJob = null;
     this._logDataReceived = true;
@@ -109,30 +105,13 @@ class RiseDataWeather extends CacheMixin( PolymerElement ) {
   ready() {
     super.ready();
 
-    if ( RisePlayerConfiguration.isConfigured()) {
-      this._init();
-    } else {
-      const init = () => this._init();
-
-      window.addEventListener( "rise-components-ready", init, { once: true });
-    }
+    super.initCache({
+      name: this.tagName.toLowerCase()
+    });
   }
 
   _init() {
-    const name = this.tagName.toLowerCase();
-
-    super.initCache({
-      name
-    });
-    super.initLogger({
-      name,
-      id: this.id,
-      version
-    });
-
-    this.addEventListener( RiseDataWeather.EVENT_START, this._handleStart, { once: true });
-
-    this._sendWeatherEvent( RiseDataWeather.EVENT_CONFIGURED );
+    super._init();
   }
 
   _getUrl() {
@@ -144,14 +123,6 @@ class RiseDataWeather extends CacheMixin( PolymerElement ) {
     url += "&name=" + encodeURIComponent( this.fullAddress );
 
     return url;
-  }
-
-  _sendWeatherEvent( eventName, detail = {}) {
-    const event = new CustomEvent( eventName, {
-      bubbles: true, composed: true, detail
-    });
-
-    this.dispatchEvent( event );
   }
 
   _refresh( interval ) {
@@ -197,18 +168,18 @@ class RiseDataWeather extends CacheMixin( PolymerElement ) {
 
       this._setWeatherData( data );
 
-      this._sendWeatherEvent( RiseDataWeather.EVENT_DATA_UPDATE, this.weatherData );
+      this._sendEvent( RiseDataWeather.EVENT_DATA_UPDATE, this.weatherData );
     } catch ( e ) {
       super.log( "error", "data error", { error: e.message });
 
-      this._sendWeatherEvent( RiseDataWeather.EVENT_DATA_ERROR, e );
+      this._sendEvent( RiseDataWeather.EVENT_DATA_ERROR, e );
     }
 
     this._refresh( RiseDataWeather.FETCH_CONFIG.REFRESH );
   }
 
   _handleStart() {
-    super.log( "info", "start received" );
+    super._handleStart();
 
     RisePlayerConfiguration.DisplayData.onDisplayAddress(( displayAddress ) => {
       this.displayAddress = displayAddress;
@@ -231,7 +202,7 @@ class RiseDataWeather extends CacheMixin( PolymerElement ) {
     } else {
       super.log( "error", message, this.displayAddress );
 
-      this._sendWeatherEvent( RiseDataWeather.EVENT_DATA_ERROR, message );
+      this._sendEvent( RiseDataWeather.EVENT_DATA_ERROR, message );
     }
   }
 
@@ -248,7 +219,7 @@ class RiseDataWeather extends CacheMixin( PolymerElement ) {
       this._weatherRequestRetryCount = 0;
 
       super.log( "error", "request error", { error: err ? err.message : null });
-      this._sendWeatherEvent( RiseDataWeather.EVENT_REQUEST_ERROR );
+      this._sendEvent( RiseDataWeather.EVENT_REQUEST_ERROR );
 
       this._refresh( RiseDataWeather.FETCH_CONFIG.COOLDOWN );
     }
