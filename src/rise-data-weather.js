2620feb232825fd8ed111799c814a5e0a3ae1a2f
Alex Deaconu <alex.deaconu@gmail.com>
2019-07-09 15:18:01 -0400

    Send postal code/zip directly if available (#39)
    Send Postal Code/Zip and Country for US/CA
    Do not send other address fields

--- src/rise-data-weather.js
+++ src/rise-data-weather.js
@@ -106,17 +106,22 @@ class RiseDataWeather extends CacheMixin( RiseElement ) {
 
     let resp = [];
 
-    if ( displayAddress.city ) {
-      resp.push( displayAddress.city );
-    }
-    if ( displayAddress.province ) {
-      resp.push( displayAddress.province );
-    }
-    if ( displayAddress.postalCode ) {
+    if (( displayAddress.country === "US" || displayAddress.country === "CA" ) && displayAddress.postalCode ) {
       resp.push( displayAddress.postalCode );
-    }
-    if ( displayAddress.country ) {
       resp.push( displayAddress.country );
+    } else {
+      if ( displayAddress.city ) {
+        resp.push( displayAddress.city );
+      }
+      if ( displayAddress.province ) {
+        resp.push( displayAddress.province );
+      }
+      if ( displayAddress.postalCode ) {
+        resp.push( displayAddress.postalCode );
+      }
+      if ( displayAddress.country ) {
+        resp.push( displayAddress.country );
+      }
     }
 
     return resp.join( "," );
